c4a5d52465b4aa222ea73825bf39a52841af7105
Hassan <baashiyare11@gmail.COM>
2025-10-20 18:53:59 -0500

    Implement Edit Profile pre-fill and submission, add New Post form handler, and add smooth modal transitions

--- scripts/index.js
+++ scripts/index.js
@@ -3,14 +3,25 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileClose = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
 
 // Open Edit Profile Modal
-editProfileBtn.addEventListener("click", () => {
+editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 
 // Close Edit Profile Modal
-editProfileClose.addEventListener("click", () => {
+editProfileClose.addEventListener("click", function () {
   editProfileModal.classList.remove("modal_is-opened");
 });
 
@@ -19,13 +30,38 @@ editProfileClose.addEventListener("click", () => {
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostClose = newPostModal.querySelector(".modal__close-btn");
+const addCardFormElement = newPostModal.querySelector(".modal__form");
+const nameInput = newPostModal.querySelector("#caption");
+const linkInput = newPostModal.querySelector("#image-input");
 
 // Open New Post Modal
-newPostBtn.addEventListener("click", () => {
+newPostBtn.addEventListener("click", function () {
   newPostModal.classList.add("modal_is-opened");
 });
 
 // Close New Post Modal
-newPostClose.addEventListener("click", () => {
+newPostClose.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+// Handle Edit Profile form submission
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+
+// Handle New Post form submission
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+  console.log("Image Link:", linkInput.value);
+  console.log("Caption:", nameInput.value);
+
+  newPostModal.classList.remove("modal_is-opened");
+}
+
+addCardFormElement.addEventListener("submit", handleAddCardSubmit);
